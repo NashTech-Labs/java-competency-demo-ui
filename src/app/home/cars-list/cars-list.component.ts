a0147825bd46808a42e9bf641070e17440259c80
ChilledCoder <rahul.sinha@knoldus.com>
2023-08-01 13:44:38 +0530

    added angular docs

--- src/app/home/cars-list/cars-list.component.ts
+++ src/app/home/cars-list/cars-list.component.ts
@@ -1,5 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CardService } from '../services/service.service';
+/**
+ * CarsListComponent displays a list of cars fetched from the CardService based on the current page number.
+ *
+ * The component is responsible for fetching and managing the data to display a paginated list of cars.
+ * It allows navigation to the next and previous pages of cars.
+ */
+
+import {Component, Input, OnInit} from '@angular/core';
+import {CardService} from '../services/service.service';
 
 @Component({
     selector: 'app-cars-list',
@@ -7,29 +14,55 @@ import { CardService } from '../services/service.service';
     styleUrls: ['./cars-list.component.scss']
 })
 export class CarsListComponent implements OnInit {
+    /**
+     * An array containing the cars fetched from the service.
+     */
     cars: any;
+
+    /**
+     * The current page number for paginated data.
+     */
     pageNumber = 1;
 
-    constructor(private carsData: CardService) {}
+    /**
+     * Creates an instance of CarsListComponent.
+     * @param carsData - The CardService instance used to fetch car data.
+     */
+    constructor(private carsData: CardService) {
+    }
 
+    /**
+     * Lifecycle hook called after the component has been initialized.
+     * It triggers the initial data fetch.
+     */
     ngOnInit() {
         this.getData();
     }
 
-    // Fetches data from the CardService based on the current page number.
+    /**
+     * Fetches data from the CardService based on the current page number.
+     * The fetched data is stored in the 'cars' property.
+     */
     private getData() {
         this.carsData.getData(this.pageNumber).subscribe((data) => {
             this.cars = data;
         });
     }
 
-    // Navigates to the next page of cars.
+    /**
+     * Navigates to the next page of cars.
+     * Increments the 'pageNumber' property and fetches the new data.
+     */
     nextPage() {
         this.pageNumber++;
         this.getData();
     }
 
-    // Navigates to the previous page of cars if not on the first page.
+    /**
+     * Navigates to the previous page of cars if not on the first page.
+     * Decrements the 'pageNumber' property and fetches the new data.
+     * If already on the first page (pageNumber === 1), logs a message to the console.
+     */
     previousPage() {
         if (this.pageNumber > 1) {
             this.pageNumber--;
