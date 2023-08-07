35f670dcda404e6ca9dc53d32f993c21267597ce
mathurakshat <91469568+mathurakshat@users.noreply.github.com>
2023-08-07 18:30:28 +0530

    Fixed home.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 import { constants } from "../constants/constants";
-import {ThemePalette} from "@angular/material/core";
-
+import { AvailableCloudOptions } from "../shared/module/cloud-item-details.model";
 
 /**
  * Represents the HomeComponent, the landing page of the application.
@@ -11,14 +10,9 @@ import {ThemePalette} from "@angular/material/core";
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-
-
 export class HomeComponent {
   Title: string = "Java UI Demo";
   Description: string = "A Reactive Application";
-  color: ThemePalette
-
-
 
   /**
    * The URL for the NashTech logo, obtained from the constants file.
@@ -28,11 +22,20 @@ export class HomeComponent {
   /**
    * An array of cloud options with their labels and paths.
    */
-  cloudOptions = [
-    { label: "Azure", path: "./azure", icon: constants.azureIcon, color: 'accent', isSelected:true },
-    { label: "GCP", path: "./gcp", icon: constants.gcpIcon, color: 'accent', isSelected:false  },
+  cloudOptions: AvailableCloudOptions[] = [
+    {
+      label: "Azure",
+      path: "./azure",
+      icon: constants.azureIcon,
+      color: "accent",
+      isSelected: true,
+    },
+    {
+      label: "GCP",
+      path: "./gcp",
+      icon: constants.gcpIcon,
+      color: "accent",
+      isSelected: false,
+    },
   ];
-
-
-
 }
