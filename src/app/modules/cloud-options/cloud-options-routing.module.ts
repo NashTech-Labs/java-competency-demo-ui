18d3555c9f19629fd996c6b73329062697820227
mathurakshat <91469568+mathurakshat@users.noreply.github.com>
2023-08-24 15:29:06 +0530

    Added routing for new components

--- src/app/modules/cloud-options/cloud-options-routing.module.ts
+++ src/app/modules/cloud-options/cloud-options-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CloudOptionsComponent } from "./cloud-options.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: CloudOptionsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CloudOptionsRoutingModule { }
+export class CloudOptionsRoutingModule {}
