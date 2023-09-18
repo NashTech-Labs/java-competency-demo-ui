dca9e8c278d75a62d74cfe9b77687fcd3850457e
Agnibhas Chattopadhyay <agnibhas.chattopadhyay@knoldus.com>
2023-09-18 13:14:06 +0530

    Revert "header changes"
    This reverts commit 66135557e2e8bd1355a576867fcdfa6895cf0e5f.

--- src/app/dashboard/components/header/header.component.ts
+++ src/app/dashboard/components/header/header.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { constants } from "src/app/constants/constants";
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  })
+  styleUrls: ['./header.component.scss']
+})
 export class HeaderComponent {
-  cartItemCount: number = 0;
-  nashTechLogoUrl: string = constants.nashTechLogoUrl;
 
 }
