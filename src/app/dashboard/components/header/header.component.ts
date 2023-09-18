66135557e2e8bd1355a576867fcdfa6895cf0e5f
Agnibhas Chattopadhyay <agnibhas.chattopadhyay@knoldus.com>
2023-09-18 12:46:05 +0530

    header changes

--- src/app/dashboard/components/header/header.component.ts
+++ src/app/dashboard/components/header/header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { constants } from "src/app/constants/constants";
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
-})
+  styleUrls: ['./header.component.scss'],
+  })
 export class HeaderComponent {
+  cartItemCount: number = 0;
+  nashTechLogoUrl: string = constants.nashTechLogoUrl;
 
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,10 +5,13 @@ import { DashboardComponent } from "./dashboard.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { SidenavComponent } from "./components/sidenav/sidenav.component";
 import { MaterialModule } from "../shared/module/material.module";
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import {CloudOptionsModule} from "../modules/cloud-options/cloud-options.module";
 import {SharedModule} from "../shared/shared.module";
 @NgModule({
   declarations: [DashboardComponent, HeaderComponent, SidenavComponent],
-    imports: [CommonModule, DashboardRoutingModule, MaterialModule, CloudOptionsModule, SharedModule],
+      imports: [CommonModule, DashboardRoutingModule, MaterialModule, CloudOptionsModule, SharedModule, MatBadgeModule, MatButtonModule, MatIconModule],
 })
 export class DashboardModule {}
