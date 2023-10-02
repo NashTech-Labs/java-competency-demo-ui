2377f96b3d1465fee04764f872fe18d6d32b9163
mathurakshat <91469568+mathurakshat@users.noreply.github.com>
2023-10-02 13:16:43 +0530

    Fixed CORS

--- src/app/shared/components/tabular-view/tabular-view.component.ts
+++ src/app/shared/components/tabular-view/tabular-view.component.ts
@@ -1,20 +1,20 @@
-import {Component, ViewChild} from '@angular/core';
-import {ColDef, ColGroupDef} from "ag-grid-community";
-import {AgGridAngular} from "ag-grid-angular";
-import {Router} from "@angular/router";
-import {CarDataService} from "../../../dashboard/service/car-data.service";
-import { Subscription } from 'rxjs';
-import { ActionColumnComponent } from '../action-column/action-column.component';
-import {FormControl} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
+import { Component, ViewChild } from "@angular/core";
+import { ColDef, ColGroupDef } from "ag-grid-community";
+import { AgGridAngular } from "ag-grid-angular";
+import { Router } from "@angular/router";
+import { CarDataService } from "../../../dashboard/service/car-data.service";
+import { CarDetailsService } from "../../services/car-details.service";
+import { Subscription } from "rxjs";
+import { ActionColumnComponent } from "../action-column/action-column.component";
+import { FormControl } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-tabular-view',
-  templateUrl: './tabular-view.component.html',
-  styleUrls: ['./tabular-view.component.scss']
+  selector: "app-tabular-view",
+  templateUrl: "./tabular-view.component.html",
+  styleUrls: ["./tabular-view.component.scss"],
 })
 export class TabularViewComponent {
-
   tableHeaders: ColDef[] = [];
   isLoading = false;
   carColumnDef: (ColDef | ColGroupDef)[] = [];
@@ -22,9 +22,9 @@ export class TabularViewComponent {
   allTableHeaders: any[] = [];
   carDataSubscription$!: Subscription;
   carData: any[] = [];
-  filteredCarData : any [] = [];
-  carBrands : string[] = [];
-  carBrandsData: any[]=[];
+  filteredCarData: any[] = [];
+  carBrands: string[] = [];
+  carBrandsData: any[] = [];
   selectedCarBrands: string[] = [];
   carBrandControl = new FormControl();
   public defaultColDef: ColDef = {
@@ -36,20 +36,25 @@ export class TabularViewComponent {
     resizable: true,
     suppressMenu: true,
     wrapHeaderText: true,
-    autoHeaderHeight: true
+    autoHeaderHeight: true,
   };
 
   public autoGroupColumnDef: ColDef = {
     minWidth: 200,
   };
   public overlayLoadingTemplate =
-      '<span class="ag-overlay-loading-center">Please wait while your rows are loading</span>';
+    '<span class="ag-overlay-loading-center">Please wait while your rows are loading</span>';
   public overlayNoRowsTemplate =
-      '<span style="padding: 10px; border: 2px solid #444; background: lightgoldenrodyellow;">This is a custom \'no rows\' overlay</span>';
+    "<span style=\"padding: 10px; border: 2px solid #444; background: lightgoldenrodyellow;\">This is a custom 'no rows' overlay</span>";
 
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
-  constructor(private router: Router, private shrinkService:CarDataService, private httpClient: HttpClient){}
+  constructor(
+    private router: Router,
+    private shrinkService: CarDataService,
+    private httpClient: HttpClient,
+    private carDetails: CarDetailsService,
+  ) {}
 
   ngOnInit() {
     this.getCarBrands();
@@ -58,7 +63,6 @@ export class TabularViewComponent {
   pagination: boolean = true;
   paginationPageSize: number = 10;
 
-
   getCarData() {
     this.isLoading = true;
     this.carDataSubscription$ = this.shrinkService
@@ -66,22 +70,88 @@ export class TabularViewComponent {
       .subscribe((res) => {
         if (res) {
           this.carColumnDef = [
-            {field: 'brand_id', headerName:('Brand_Id'),  colId: 'brand_id',minWidth:180, filter: 'agTextColumnFilter', suppressMenu: true, unSortIcon: true},
-            {field: 'brand_name', headerName: ('Brand_Name'), colId: 'brand_name', minWidth:210, filter: 'agTextColumnFilter', suppressMenu: true, unSortIcon: true},
-            {field: 'model', headerName: ('Model'), colId: 'model', filter: 'agTextColumnFilter', suppressMenu: true, unSortIcon: true},
-            {field: 'year', headerName: ('Year'), colId: 'year', filter: 'agTextColumnFilter', suppressMenu: true, unSortIcon: true},
-            {field: 'color', headerName: ('Color'), colId: 'color',minWidth:210, filter: 'agTextColumnFilter', suppressMenu: true, unSortIcon: true},
-            {field: 'mileage', headerName: ('Mileage'), colId: 'mileage', filter: 'agTextColumnFilter', suppressMenu: true, unSortIcon: true},
-            {field: 'price', headerName: ('Price'), colId: 'price',minWidth:210, filter: 'agTextColumnFilter', suppressMenu: true, unSortIcon: true},
-            {field: 'location', headerName: ('Location'), colId: 'location', filter: 'agTextColumnFilter', suppressMenu: true, unSortIcon: true},
-            {field: 'Action', headerName: ('Action'), colId: 'Action',cellRenderer: ActionColumnComponent, floatingFilter: false}
-          ]
+            {
+              field: "brand_id",
+              headerName: "Brand_Id",
+              colId: "brand_id",
+              minWidth: 180,
+              filter: "agTextColumnFilter",
+              suppressMenu: true,
+              unSortIcon: true,
+            },
+            {
+              field: "brand_name",
+              headerName: "Brand_Name",
+              colId: "brand_name",
+              minWidth: 210,
+              filter: "agTextColumnFilter",
+              suppressMenu: true,
+              unSortIcon: true,
+            },
+            {
+              field: "model",
+              headerName: "Model",
+              colId: "model",
+              filter: "agTextColumnFilter",
+              suppressMenu: true,
+              unSortIcon: true,
+            },
+            {
+              field: "year",
+              headerName: "Year",
+              colId: "year",
+              filter: "agTextColumnFilter",
+              suppressMenu: true,
+              unSortIcon: true,
+            },
+            {
+              field: "color",
+              headerName: "Color",
+              colId: "color",
+              minWidth: 210,
+              filter: "agTextColumnFilter",
+              suppressMenu: true,
+              unSortIcon: true,
+            },
+            {
+              field: "mileage",
+              headerName: "Mileage",
+              colId: "mileage",
+              filter: "agTextColumnFilter",
+              suppressMenu: true,
+              unSortIcon: true,
+            },
+            {
+              field: "price",
+              headerName: "Price",
+              colId: "price",
+              minWidth: 210,
+              filter: "agTextColumnFilter",
+              suppressMenu: true,
+              unSortIcon: true,
+            },
+            {
+              field: "location",
+              headerName: "Location",
+              colId: "location",
+              filter: "agTextColumnFilter",
+              suppressMenu: true,
+              unSortIcon: true,
+            },
+            {
+              field: "Action",
+              headerName: "Action",
+              colId: "Action",
+              cellRenderer: ActionColumnComponent,
+              floatingFilter: false,
+            },
+          ];
 
-          this.carData = res.map(item => {
+          this.carData = res.map((item) => {
             return { ...item, ...this.carColumnDef };
           });
 
-          console.log("data is " , JSON.stringify(this.carData));
+          console.log("data is ", JSON.stringify(this.carData));
 
           this.carData = res;
 
@@ -93,57 +163,46 @@ export class TabularViewComponent {
         }
         this.isLoading = false;
       });
-    }
-
-  addData() {
-    const apiUrl = 'http://35.193.88.251/v1/data';  // Replace with your actual API endpoint URL
-    this.httpClient.post(apiUrl, {})
-        .subscribe(
-            (response) => {
-              console.log('POST request successful:', response);
-              // Handle any success actions or notifications here
-            },
-            (error) => {
-              console.error('Error making POST request:', error);
-              // Handle any error actions or notifications here
-            }
-        );
   }
 
-
+  addBulkData() {
+    this.carDetails
+      .addBulkData()
+      .subscribe((data) => console.log("Bulk data uploaded successfully."));
+  }
 
   getCarBrands() {
     this.isLoading = true;
 
     this.shrinkService.getBrands().subscribe(
-        (brands: string[]) => {
-          this.carBrandsData = [...brands];
-          this.carBrandsData.forEach((item :any) => {
-            this.carBrands.push(item.brand);
-          });
+      (brands: string[]) => {
+        this.carBrandsData = [...brands];
+        this.carBrandsData.forEach((item: any) => {
+          this.carBrands.push(item.brand);
+        });
 
-          this.selectedCarBrands = this.carBrands.slice(0, 2);
-          this.carBrandControl.setValue(this.selectedCarBrands);
-          this.getCarData();
-          this.isLoading = false;
-        },
-        (error) => {
-          console.error('Error fetching car brands', error);
-          this.isLoading = false;
-        }
+        this.selectedCarBrands = this.carBrands.slice(0, 2);
+        this.carBrandControl.setValue(this.selectedCarBrands);
+        this.getCarData();
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error("Error fetching car brands", error);
+        this.isLoading = false;
+      },
     );
   }
-  setHeadersForBulkShrink(event: any){
+  setHeadersForBulkShrink(event: any) {
     this.updatedCarHeaders = [];
     this.updatedCarHeaders = this.setTableHeaders(event, this.allTableHeaders);
     this.carColumnDef = this.updatedCarHeaders;
   }
 
-  setTableHeaders(event: any, tableHeaders: ColDef[]): ColDef<any>[]{
+  setTableHeaders(event: any, tableHeaders: ColDef[]): ColDef<any>[] {
     let updatedHeaders: ColDef<any>[] = [];
-    if(event.length > 0){
-      tableHeaders.forEach((item:ColDef) => {
-        if(event.includes(item.colId)){
+    if (event.length > 0) {
+      tableHeaders.forEach((item: ColDef) => {
+        if (event.includes(item.colId)) {
           updatedHeaders.push(item);
         }
       });
@@ -153,20 +212,18 @@ export class TabularViewComponent {
 
   setTableDataForSelectedBrands() {
     console.log("selected car brands" + this.selectedCarBrands);
-     this.selectedCarBrands = this.carBrandControl.value;
-      if (this.selectedCarBrands.length >0){
-        this.carData = this.filteredCarData.filter(item =>
-            this.selectedCarBrands.includes(item.brand_name)
-        );
-      }
-      else {
-        this.getCarData();
-      }
+    this.selectedCarBrands = this.carBrandControl.value;
+    if (this.selectedCarBrands.length > 0) {
+      this.carData = this.filteredCarData.filter((item) =>
+        this.selectedCarBrands.includes(item.brand_name),
+      );
+    } else {
+      this.getCarData();
+    }
   }
 
   onCarBrandSelectionChange(event: any) {
     this.selectedCarBrands = event.value;
     this.setTableDataForSelectedBrands();
   }
-
 }
