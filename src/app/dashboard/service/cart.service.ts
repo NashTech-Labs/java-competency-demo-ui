f043d3371f7b91f9fd1f98516146d1cf2ba5e2d4
DeepaMittal <deepamittalmca@gmail.com>
2023-10-06 03:30:08 +0530

    added data in local storage

--- src/app/dashboard/service/cart.service.ts
+++ src/app/dashboard/service/cart.service.ts
@@ -5,8 +5,14 @@ import { BehaviorSubject, Observable } from 'rxjs';
     providedIn: 'root'
 })
 export class CartService {
-    private cartItemCountSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
-    cartItemCount$: Observable<number> = this.cartItemCountSubject.asObservable();
+    private cartItemCountSubject: BehaviorSubject<number>;
+    cartItemCount$: Observable<number>;
+
+    constructor() {
+        const initialCount = parseInt(localStorage.getItem('cartCount') || '0', 10);
+        this.cartItemCountSubject = new BehaviorSubject<number>(initialCount);
+        this.cartItemCount$ = this.cartItemCountSubject.asObservable();
+    }
 
     getCartItemCount(): number {
         return this.cartItemCountSubject.value;
@@ -15,5 +21,6 @@ export class CartService {
     incrementCartItemCount(): void {
         const currentCount = this.cartItemCountSubject.value;
         this.cartItemCountSubject.next(currentCount + 1);
+        localStorage.setItem('cartCount', (currentCount + 1).toString());
     }
 }
