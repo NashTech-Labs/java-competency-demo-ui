2b1470bd9c63421d2ef4d9dcdc893f29fa6e9ee8
mathurakshat <91469568+mathurakshat@users.noreply.github.com>
2023-10-10 13:54:45 +0530

    header fixed partially

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,40 +1,38 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { CartItem } from "../module/cart-item.model";
-import {BehaviorSubject, map, Observable} from "rxjs";
+import { BehaviorSubject, map, Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class CartService {
-
   private cartItemCountSubject: BehaviorSubject<number>;
   cartItemCount$: Observable<number>;
-  private getCartItemUrl : string =  "http://localhost:9094/cart/get";
+  private getCartItemUrl: string = "http://localhost:9094/cart/get";
 
   constructor(private httpClient: HttpClient) {
-    const initialCount = parseInt(localStorage.getItem('cartCount') || '0', 10);
+    const initialCount = parseInt(localStorage.getItem("cartCount") || "0", 10);
     this.cartItemCountSubject = new BehaviorSubject<number>(initialCount);
     this.cartItemCount$ = this.cartItemCountSubject.asObservable();
   }
   getCartItems(): Observable<CartItem[]> {
     const url = `${this.getCartItemUrl}?userId=1652`;
     return this.httpClient.get<any[]>(url).pipe(
-        map(response => {
-          if (response != null) {
-            return response;
-          } else {
-            console.error("Failed to get cart items");
-            return [];
-          }
-        })
+      map((response) => {
+        if (response != null) {
+          return response;
+        } else {
+          console.error("Failed to get cart items");
+          return [];
+        }
+      }),
     );
   }
 
   incrementCartItemCount(): void {
     const currentCount = this.cartItemCountSubject.value;
     this.cartItemCountSubject.next(currentCount + 1);
-    localStorage.setItem('cartCount', (currentCount + 1).toString());
+    localStorage.setItem("cartCount", (currentCount + 1).toString());
   }
-
 }
