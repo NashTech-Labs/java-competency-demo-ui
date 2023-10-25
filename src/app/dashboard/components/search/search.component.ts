4379e59b27bd347335fad6a083bf9e41b96e1eda
Agnibhas Chattopadhyay <agnibhas.chattopadhyay@nashtechglobal.com>
2023-10-25 16:27:32 +0530

    modify search bar design

--- src/app/dashboard/components/search/search.component.ts
+++ src/app/dashboard/components/search/search.component.ts
@@ -1,49 +1,32 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
   @ViewChild('dropdownButton') dropdownButton!: ElementRef;
-
   @ViewChild('dropdownContent') dropdownContent!: ElementRef;
 
-
-
-  items: string[] = ['CarId', 'Mileage', 'Price', 'Colour'];
-
+  items: string[] = ['All', 'Brand', 'Price','Mileage'];
   filteredItems: string[] = [];
-
   searchTerm: string = '';
-
-
+  selectedCategory: string = 'All';
 
   ngOnInit() {
 
-    this.filteredItems = this.items;
+    this.filteredItems = this.items.filter(item => item !== this.selectedCategory);
 
   }
-
-
-
   filterItems() {
-
     this.filteredItems = this.items.filter(item =>
-
         item.toLowerCase().includes(this.searchTerm.toLowerCase())
-
     );
-
   }
-
-
-
   menuItemClicked(item: string) {
-
     console.log('Clicked:', item);
-
+    this.selectedCategory = item;
+    this.filteredItems = this.items.filter(item => item !== this.selectedCategory);
   }
-
-
 }
