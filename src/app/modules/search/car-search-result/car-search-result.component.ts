17bab8583106194aa77fed095a956f5ad367031c
mathurakshat <91469568+mathurakshat@users.noreply.github.com>
2023-10-26 12:20:23 +0530

    search result

--- src/app/modules/search/car-search-result/car-search-result.component.ts
+++ src/app/modules/search/car-search-result/car-search-result.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { BrandsList } from "../../../../assets/data/brandsListJson";
 
 @Component({
-  selector: 'app-car-search-result',
-  templateUrl: './car-search-result.component.html',
-  styleUrls: ['./car-search-result.component.scss']
+  selector: "app-car-search-result",
+  templateUrl: "./car-search-result.component.html",
+  styleUrls: ["./car-search-result.component.scss"],
 })
 export class CarSearchResultComponent {
-
+  carsData: CarDetails1[] = [];
+  ngOnInit() {
+    this.carsData = BrandsList;
+  }
+}
+interface CarDetails1 {
+  brand_id: number;
+  brand_name: string;
+  model: string;
+  year: number;
+  color: string;
+  mileage: number;
+  price: string;
+  location: string;
 }
--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { SearchRoutingModule } from './search-routing.module';
-import { CarSearchResultComponent } from './car-search-result/car-search-result.component';
-import {DashboardModule} from "../../dashboard/dashboard.module";
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SearchRoutingModule } from "./search-routing.module";
+import { CarSearchResultComponent } from "./car-search-result/car-search-result.component";
+import { DashboardModule } from "../../dashboard/dashboard.module";
+import { HomeModule } from "../../home/home.module";
 
 @NgModule({
-  declarations: [
-    CarSearchResultComponent
-  ],
-  imports: [
-    CommonModule,
-    SearchRoutingModule,
-    DashboardModule
-  ]
+  declarations: [CarSearchResultComponent],
+  imports: [CommonModule, SearchRoutingModule, DashboardModule, HomeModule],
 })
-export class SearchModule { }
+export class SearchModule {}
