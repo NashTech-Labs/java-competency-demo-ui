95bccb3beab2aa0e90ec783e53b446ce2f167531
Agnibhas Chattopadhyay <agnibhas.chattopadhyay@nashtechglobal.com>
2023-10-27 12:35:42 +0530

    Add to Cart button

--- src/app/modules/search/car-search-result/car-search-result.component.ts
+++ src/app/modules/search/car-search-result/car-search-result.component.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core";
 import { BrandsList } from "../../../../assets/data/brandsListJson";
+import {CartService} from "../../../shared/services/cart.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {ICellRendererParams} from "ag-grid-community";
 
 @Component({
   selector: "app-car-search-result",
@@ -7,11 +10,40 @@ import { BrandsList } from "../../../../assets/data/brandsListJson";
   styleUrls: ["./car-search-result.component.scss"],
 })
 export class CarSearchResultComponent {
+  constructor(    private cartService: CartService,
+                  private snackBar: MatSnackBar,
+
+  ) {
+  }
   carsData: CarDetails1[] = [];
   ngOnInit() {
     this.carsData = BrandsList;
   }
+
+  public bulkEventCellValue!: any;
+  public carId!: string;
+
+  agInit(params: ICellRendererParams): void {
+    this.bulkEventCellValue = params;
+    this.carId = params.data.carId;
+  }
+  addToCart(): void {
+    this.cartService.addToCart(this.carId).subscribe((response) => {
+      if (response != null) {
+        this.cartService.incrementCartItemCount();
+        this.showSnackBar("Item has been added to the cart");
+      } else {
+        console.error("Failed to add to cart:");
+      }
+    });
+  }
+  private showSnackBar(message: string) {
+    this.snackBar.open(message, "Close", {
+      duration: 3000,
+    });
+  }
 }
+
 interface CarDetails1 {
   brand_id: number;
   brand_name: string;
